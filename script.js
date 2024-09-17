61415b2a04eafceef2a2eb4989f273eff59cfd65
consumoedeeste <adrian.perezrosa132024@gmail.com>
2024-09-17 09:31:51 -0400

    first upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function calcularDiferencia() {
+    let referencia = parseFloat(document.getElementById('referencia').value);
+    let actuales = parseFloat(document.getElementById('actuales').value);
+    
+    if (isNaN(referencia) || isNaN(actuales) || referencia < 0 || actuales < 0 || actuales < referencia) {
+        alert("Por favor, ingrese valores válidos.");
+        return;
+    }
+    
+    // Calcular la diferencia
+    let consumo = actuales - referencia;
+    
+    // Colocar el resultado en la casilla de consumo
+    document.getElementById('consumo').value = consumo;
+    
+    // Llamar a la función que calcula el total de la factura
+    calcularFactura(consumo);
+}
+
+function calcularFactura(consumo) {
+    let cargoFijo = 127.83;
+    let tarifaBaja = 6.17;
+    let tarifaAlta = 8.71;
+    let total = 0;
+
+    if (consumo <= 200) {
+        total = cargoFijo + (consumo * tarifaBaja);
+    } else {
+        total = cargoFijo + (200 * tarifaBaja) + ((consumo - 200) * tarifaAlta);
+    }
+
+    document.getElementById('resultado').innerText = `El total de la factura es RD$ ${total.toFixed(2)}`;
+}
